f700037c4472d6cd0ad121e86d9c2ce96e418c76
Emirhan Dilber <edilberfb@gmail.com>
2023-09-02 13:02:03 +0300

    solution

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace degisken
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int deger = 2;
+            Console.WriteLine(deger);
+            byte b = 5;
+            sbyte c = 5;
+            uint ui=1;
+            DateTime zaman1 = new DateTime(2002,07,12);
+            
+
+        }
+    }
+
+}
